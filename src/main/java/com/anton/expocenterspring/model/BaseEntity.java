1a99482978a2a2ca451dc4f2aae15fc94b7ef790
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-23 01:57:34 +0200

    Added Base Entity

---
+++ src/main/java/com/anton/expocenterspring/model/BaseEntity.java
@@ -0,0 +1,16 @@
+package com.anton.expocenterspring.model;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public class BaseEntity {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}

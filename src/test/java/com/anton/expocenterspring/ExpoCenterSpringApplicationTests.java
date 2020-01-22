114d29bb48b18f3b4e0a2f3317ab5c70f3d61242
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-23 01:39:36 +0200

    Initial commit

---
+++ src/test/java/com/anton/expocenterspring/ExpoCenterSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.anton.expocenterspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpoCenterSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

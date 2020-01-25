910beefc19abb691114bcb1a2845bda6333efbe6
Anton <ssa.ssa.ssa11@gmail.com>
2020-01-25 03:53:04 +0200

    added UserPrincipal entity.

--- src/main/java/com/anton/expocenterspring/repositories/UserRepository.java
+++ src/main/java/com/anton/expocenterspring/repositories/UserRepository.java
@@ -4,4 +4,5 @@ import com.anton.expocenterspring.model.User;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsername(String username);
 }

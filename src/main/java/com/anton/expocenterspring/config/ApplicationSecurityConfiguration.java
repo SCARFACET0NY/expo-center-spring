521160cb06f90ade65e2ea8d11fc523127bd9d2d
Anton <ssa.ssa.ssa11@gmail.com>
2020-02-03 13:34:12 +0200

    added protection to admin methods

--- src/main/java/com/anton/expocenterspring/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/anton/expocenterspring/config/ApplicationSecurityConfiguration.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -17,7 +16,6 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
     private final UserService userService;
 
--- src/main/java/com/anton/expocenterspring/controllers/AdminController.java
+++ src/main/java/com/anton/expocenterspring/controllers/AdminController.java
@@ -4,6 +4,8 @@ import com.anton.expocenterspring.auth.UserPrincipal;
 import com.anton.expocenterspring.model.Exposition;
 import com.anton.expocenterspring.services.ExpositionService;
 import com.anton.expocenterspring.services.HallService;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 
 @Controller
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class AdminController {
     private final HallService hallService;
     private final ExpositionService expositionService;
@@ -23,6 +26,7 @@ public class AdminController {
     }
 
     @GetMapping("/admin")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String getAdminPage(Model model) {
         model.addAttribute("user",
                 ((UserPrincipal) SecurityContextHolder.getContext().getAuthentication().getPrincipal()).getUser());
@@ -31,6 +35,7 @@ public class AdminController {
     }
 
     @GetMapping("/admin/add")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String getAddExpositionPage(Model model) {
         model.addAttribute("exposition", new Exposition());
         model.addAttribute("halls", hallService.getAllHalls());
@@ -39,6 +44,7 @@ public class AdminController {
     }
 
     @PostMapping("/admin/add")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public String addExposition(@ModelAttribute Exposition exposition) {
         expositionService.save(exposition);
         return "redirect:/admin";

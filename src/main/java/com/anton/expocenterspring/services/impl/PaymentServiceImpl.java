a1e0c77cda4be04cf69f3e26bb55c4976584385d
Anton <ssa.ssa.ssa11@gmail.com>
2020-02-16 21:47:19 +0200

    Invalid credentials message.

--- src/main/java/com/anton/expocenterspring/services/impl/PaymentServiceImpl.java
+++ src/main/java/com/anton/expocenterspring/services/impl/PaymentServiceImpl.java
@@ -15,7 +15,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Map;

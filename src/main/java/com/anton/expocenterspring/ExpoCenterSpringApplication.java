d7619eab3806b5221b00ea60522bdb76b5098a39
Anton <ssa.ssa.ssa11@gmail.com>
2020-02-18 04:11:59 +0200

    updated data

--- src/main/java/com/anton/expocenterspring/ExpoCenterSpringApplication.java
+++ src/main/java/com/anton/expocenterspring/ExpoCenterSpringApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ExpoCenterSpringApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(ExpoCenterSpringApplication.class, args);
     }
-
 }
